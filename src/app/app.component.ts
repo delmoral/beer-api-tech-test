084a600763e735098fc3e5c2e42d1da947074225
DelMoral <delmoralvera@gmail.com>
2023-10-17 20:19:58 +0200

    Added input to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BeerResourceService } from './services/beer-resource.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'beer-api-tech-test';
+export class AppComponent implements OnInit {
+  beersList: any;
+  query = '';
+
+  constructor(private beerResource: BeerResourceService) { }
+
+  ngOnInit() {
+    this.beerResource.queryBeers().subscribe(response => {
+      this.beersList = response;
+    })
+  }
 }
